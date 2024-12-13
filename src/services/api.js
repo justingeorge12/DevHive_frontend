52871b530ab18e17a2eec6cc033ceda558eef40c
justin george <justingeorge0407@gmail.com>
2024-12-13 11:25:29 +0530

    change in env

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,8 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
+console.log('API URL:', import.meta.env.VITE_API_URL)
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 })
